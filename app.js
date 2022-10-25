56f46c6ff0a5980d0cbc8cd6500a7b249601faee
Cherry0777 <sharatreddi71@gmail.com>
2022-10-25 17:15:48 +0530

    linked app.js to signup.html

--- app.js
+++ app.js
@@ -4,21 +4,33 @@ const mysql = require('mysql');
 const _ = require("lodash");
 var path = require('path');
 const { reset } = require("nodemon");
-
-
-/* const db = mysql.createConnection({
+var db = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
-    password : '123456',
-    database : 'nodemysql'
-}) */
-
-/* db.connect((err) => {
+    password : 'mysql',
+    database : 'puparazzi'
+})
+db.connect(function(err) {
     if(err){
         throw err;
     }
-    console.log('MySQL connected...')
-}) */
+    console.log('MySQL connected...');
+        var pupid=100;
+        var pupname = pName;
+        var pupbreed = pbreed;
+        var pupage = p_age;
+        var location=loc;
+        var username=usrname;
+        var pwd=pswd;
+        var ownername=own_name;
+        var ownerage=own_age;
+        var phone=phonenum;
+        var sql="INSERT into profile values ("+pupid+",'"+pupname+"','"+ pupbreed +"', "+ pupage + ", '"+ location +"','"+username+"','" +pwd+"','"+ownername+"',"+ownerage+","+phone+")";
+            db.query(sql,function(err,result){
+              if(err) throw err;
+              console.log("record created"); 
+    });
+}) ;
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -26,7 +38,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res){
-    res.sendFile(__dirname + "/vets.html");
+    res.sendFile(__dirname + "/signup.html");
 });
 
 app.get("/:pagename", function (req, res){
